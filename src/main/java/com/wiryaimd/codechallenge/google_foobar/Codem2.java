aa06fdce1b3c9ece37adb242b7968a2b92db4005
wiryaimd <adnyasutha003@gmail.com>
2023-03-02 22:45:35 +0800

    simplfy

--- src/main/java/com/wiryaimd/codechallenge/google_foobar/Codem2.java
+++ src/main/java/com/wiryaimd/codechallenge/google_foobar/Codem2.java
@@ -11,8 +11,9 @@ public class Codem2 {
         String[] test5 = {"0.0.1", "0.1.2", "0.0.11", "0.3.0", "0.1.1", "1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0", "2.0.1", "2", "2.0", "1.0.0", "2.0.2", "2.1.0", "2.0.3", "1.0.30", "3.4", "9.9", "3.3.3", "3.4.0", "3.0.4", "3.4", "4.4", "3.4", "1.2.2", "1.1"};
         String[] test6 = {"3.4", "3.0.4", "2.0.2", "2.0.3"};
         String[] test7 = {"0.1.1", "0.1.2", "0.1"};
+        String[] test8 = {"0.5.1","0.5","50.1","0.5.2","1","2","2","3","0.0.8", "0.1.2", "0.0.11", "0.3.0", "0.1.1", "1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0", "2.0.1", "2", "2.0", "1.0.0", "2.0.2", "2.1.0", "2.0.3", "1.0.30", "3.4", "9.9", "3.3.3", "3.4.0", "3.0.4", "3.4", "4.4", "3.4", "1.2.2", "1.1"};
 
-        String[] res = solve(test5);
+        String[] res = solve(test8);
         for (int i = 0; i < res.length; i++) {
             System.out.println(res[i]);
         }
@@ -36,56 +37,21 @@ public class Codem2 {
 
     public static boolean compare(String[] ver1, String[] ver2){
 
-        // 1.1.0
-        // 1.1
-
         int min = Math.min(ver1.length, ver2.length);
-        int max = Math.max(ver1.length, ver2.length);
 
-        boolean leftLess = false;
         for (int i = 0; i < min; i++) {
             int val1 = Integer.parseInt(ver1[i]);
             int val2 = Integer.parseInt(ver2[i]);
             if (val1 < val2){
-//                leftLess = true;
-//                break;
                 return true;
             }
 
             if (val1 > val2){
-//                break;
                 return false;
             }
         }
 
-        int len = Math.abs(ver1.length - ver2.length);
-        boolean isLeft = ver1.length < ver2.length;
-        String[] highest = isLeft ? ver2 : ver1;
-        if (isLeft){
-            return true;
-        }
-//         2 - 2.0
-//        if (ver1[0].equals(ver2[0])) {
-//            boolean isZero = false;
-//            for (int i = min; i < max; i++) {
-////                int high = Integer.parseInt(highest[i]);
-//                String zero = highest[i];
-//                if (isLeft && !zero.equals("0")){
-////                    isZero = true;
-////                    break;
-//                    return true;
-//                }
-//
-////                if (isLeft && zero.equals("0")){
-////                    isZero = true;
-////                    break;
-////                }
-//            }
-////            leftLess = isZero;
-//        }
-
-//        return leftLess;
-        return false;
+        return ver1.length < ver2.length;
     }
 
 }
